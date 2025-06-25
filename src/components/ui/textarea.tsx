207ece18b8fa11bca160f35cfa9846dc2522073d
vuncse151184 <vu12ace3@gmail.com>
2025-06-25 16:33:27 +0700

    [chore] somthing less impact

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils"
 export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
+  
   return (
     <textarea
       className={cn(
